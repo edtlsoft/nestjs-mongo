a7ae3b5819ddb91a6544a620ae0da82074b1b3d5
Edward Lopez <edward.trigos@nivelics.co>
2022-01-10 17:19:10 -0500

    [add] Mongoose schema

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,3 +1,4 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
 import { ProductsController } from './controllers/products.controller';
@@ -6,8 +7,17 @@ import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
+import { Product, ProductSchema } from './entities/product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
