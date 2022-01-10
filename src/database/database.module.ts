427745313bd80fcd552ab0cf76c0b15266bc6ac6
Edward Lopez <edward.trigos@nivelics.co>
2022-01-10 17:10:49 -0500

    [add] Mongoose Module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { ConfigType } from '@nestjs/config';
 import { Module, Global } from '@nestjs/common';
 import { MongoClient } from 'mongodb';
+import { MongooseModule } from '@nestjs/mongoose';
 import config from '../config/config';
 
 const API_KEY = '12345634';
@@ -8,6 +9,28 @@ const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const {
+          connection,
+          user,
+          password,
+          host,
+          port,
+          dbName,
+        } = configService.mongo;
+
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -34,6 +57,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       },
     },
   ],
-  exports: ['API_KEY', 'MONGO'],
+  exports: ['API_KEY', 'MONGO', MongooseModule],
 })
 export class DatabaseModule {}
