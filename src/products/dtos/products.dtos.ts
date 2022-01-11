ef21b8922c404d787361664750e6ad74407b1bee
Edward Lopez <edward.trigos@nivelics.co>
2022-01-10 23:01:13 -0500

    [add] One-to-one embedded relationships

--- src/products/dtos/products.dtos.ts
+++ src/products/dtos/products.dtos.ts
@@ -7,8 +7,10 @@ import {
   IsOptional,
   Min,
   ValidateIf,
+  ValidateNested,
 } from 'class-validator';
 import { PartialType, ApiProperty } from '@nestjs/swagger';
+import { CreateCategoryDto } from './category.dtos';
 
 export class CreateProductDto {
   @IsString()
@@ -18,20 +20,29 @@ export class CreateProductDto {
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: `product's description` })
   readonly description: string;
 
   @IsNumber()
   @IsNotEmpty()
   @IsPositive()
+  @ApiProperty({ description: `product's price` })
   readonly price: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({ description: `product's stock` })
   readonly stock: number;
 
   @IsUrl()
   @IsNotEmpty()
+  @ApiProperty({ description: `product's image` })
   readonly image: string;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @ApiProperty({ description: `product's category` })
+  readonly category: CreateCategoryDto;
 }
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
